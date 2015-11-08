a10fd684c7084ecc0cc584bbb8cabed1e31c3d5b
Yizzle Yang <LilHayah@gmail.com>
2015-11-08 02:30:55 -0500

    adding updateURL

--- DouBanned.user.js
+++ DouBanned.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         DouBanned 
 // @namespace    http://lilhayah.com/
-// @version      0.1
+// @version      0.2
 // @description  Modify douban UI, take off that the annoying "1111" bubble.
 // @author       Yizzle
 // @match        http://*.douban.com/*
 // @match        https://*.douban.com/*
+// @updateURL    https://github.com/LilHayah/us4fun/raw/master/DouBanned.user.js
 // @grant        none
 // ==/UserScript==
 
