ad9cc9a0039eca611849ea396ee8408f24a3cd45
Yizzle Yang <LilHayah@gmail.com>
2015-12-05 23:54:31 -0500

    Initialize DanmakuXML, NOT FUNCTIONING NOW

---
+++ DanmakuXML.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         DanmakuXML
+// @namespace    http://lilhayah.com/
+// @version      0.1
+// @description  Download bilibili danmaku xml file
+// @author       Yizzle
+// @match        http://*.bilibili.com/video/*
+// @match        https://*.douban.com/video/*
+// @updateURL    https://github.com/LilHayah/us4fun/raw/master/DanmakuXML.user.js
+// @grant        none
+// ==/UserScript==
+
+// NOT FUNCTIONING NOW
